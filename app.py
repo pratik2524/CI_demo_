d07289ac52f71b3babde1c9501b727a7b3df1e64
Pratik <pratikmahadik2524@gmail.com>
2024-10-29 00:29:06 +0530

    FIxed Minor issues

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
+def sub(a, b): 
+    return a - b
 
+c = add(4, 5)
+d = sub(4, 1)
 
-def sub(a,b): return a-b
-
-c = add(4,5)
-d = sub(4,1)
-
-print(c,d)
+print(c, d)
